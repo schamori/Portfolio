4031915863b8e65d18db411f15680daaba5de2b6
schamori <21.5.2003.ms@gmail.com>
2026-04-24 13:28:07 +0100

    working

---
+++ video-config.js
@@ -0,0 +1,8 @@
+// CONFIGURATION FILE
+window.VIDEO_SETTINGS = {
+  "scrubSmoothing": 1.0,   // 1.0 = Instant stop (NO LAG), 0.1 = Very floaty
+  "scrubPinHeight": 5,   // SPEED CONTROL: Higher = Slower video (Try 10.0 for very slow)
+  "scrubMultiplier": 1.0,  // LEAVE AT 1.0 for normal linear speed
+  "showScrubLabel": true,
+  "scrubCaption": ""
+};
